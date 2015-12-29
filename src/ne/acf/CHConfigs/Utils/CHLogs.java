6d0d8cbbf74d5b0221c7e1c6873b1eb4c1f0288f
αdяiαиcf <Unix52@users.noreply.github.com>
2015-12-29 18:17:37 -0200

    Update CHLogs
    Updates.

--- src/ne/acf/CHConfigs/Utils/CHLogs.java
+++ src/ne/acf/CHConfigs/Utils/CHLogs.java
@@ -1,5 +1,52 @@
 package ne.acf.CHConfigs.Utils;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class CHLogs {
 
+	public String msg;
+	public boolean save;
+	
+	public CHLogs (String log, boolean salvar)
+	{
+		msg = log;
+		save = salvar; 
+		
+		Anunciar();
+		if (salvar)
+		salvarLog();
+	}
+	
+	private void Anunciar()
+	{
+		System.out.print(msg);
+	}
+	
+	 private void salvarLog() {
+		    try {
+		      File saveTo = new File("CHLog.log");
+		      if (!saveTo.exists()) {
+		        saveTo.createNewFile();
+		      }
+		      FileWriter fw = new FileWriter(saveTo, true);
+		      PrintWriter pw = new PrintWriter(fw);
+		      DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		      Date date = new Date();
+		      pw.println(dateFormat.format(date) + " - " + 
+		        msg);
+		      pw.flush();
+		      pw.close();
+		    } catch (IOException e) {
+		    	
+		      e.printStackTrace();
+		      
+		    }
+		    
+		  }
 }
